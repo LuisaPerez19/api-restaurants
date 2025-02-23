370a5b999a10754dbd71be2e37bce371a8077745
Luisa Perez <luisa.perezw3@gmail.com>
2025-02-23 16:01:21 +0000

    Add showRestaurantList method to build the API URL Add insertResults to add category and location HTML elements

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,60 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  def index
+    # category = Category.find_by(name: params[:category])
+    # if category
+    #   restaurants = category.restaurants.select(:id, :name, :address)
+    #   render json: restaurants
+    # else
+    #   render json: { error: "Category not found" }, status: :not_found
+    # end
+
+    @restaurants = Restaurant.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @restaurant.destroy
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category_id)
+  end
 end
--- app/javascript/controllers/restaurant_list_controller.js
+++ app/javascript/controllers/restaurant_list_controller.js
@@ -2,7 +2,35 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="restaurant-list"
 export default class extends Controller {
-  connect() {
-    console.log("restaurant_list connected");
+  static targets = ["searchForm", "selectedCategory", "restaurantList", "location"];
+
+  insertResults(results) {
+    const restaurantList = this.restaurantListTarget;
+    restaurantList.innerHTML = "";
+    if (results.length > 0) {
+      results.forEach((restaurant) => {
+        const listItem = document.createElement("li");
+        listItem.classList.add("list-group-item");
+        listItem.innerText = `${restaurant.name} in ${restaurant.address}`;
+        restaurantList.appendChild(listItem);
+      });
+    } else {
+      let p = document.createElement("p");
+      p = "No restaurants found! Please select another category or location";
+      restaurantList.innerHTML = p;
+    }
+  }
+
+  showRestaurantList(event) {
+    event.preventDefault();
+    const selectedCategory = this.selectedCategoryTargets.find((radio) => radio.checked)?.value; //
+    const location = this.locationTarget.value;
+    const url = `https://the-fork.api.lewagon.com/api/v1/restaurants?category=${selectedCategory}&location=${location}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        this.insertResults(data);
+      });
   }
 }
